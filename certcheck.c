c24f966051209781cc93a766d5cde0590f964ce2
Omja Das <o.das@student.unimelb.edu.au>
2018-05-23 22:38:06 +1000

    added csv handling

--- certcheck.c
+++ certcheck.c
@@ -7,14 +7,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    char path[256];
-    strcpy(path, argv[1]);
+int validate(char *, char *);
 
-    //initialise openSSL
+int main(int argc, char *argv[]) {
+    FILE *file = fopen(argv[1], "r");
+
+    // initialise openSSL
     OpenSSL_add_all_algorithms();
     ERR_load_BIO_strings();
     ERR_load_crypto_strings();
 
+    char line[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        strtok(line, "\n");
+        char *certificate, *url;
+        certificate = strtok(line, ",");
+        url = strtok(NULL, ",");
+        validate(certificate, url);
+    }
+
+    fclose(file);
+    return 0;
+}
+
+int validate(char *certificate, char *url) {
     return 0;
 }
